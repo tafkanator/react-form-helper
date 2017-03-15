728751feaead0710427ca44f14a34f85bf025f75
Taavi Sangel <taavi@stagnationlab.com>
2017-03-15 17:44:54 +0200

    improved examle visuals

--- examples/App.js
+++ examples/App.js
@@ -8,19 +8,30 @@ const handleCommentFormSubmit = (values) => {
 	// return false;
 };
 
-const handleCommentFormSuccess = () => console.log('success');
+const handleCommentFormSuccess = reset => reset();
 const handleCommentFormFail = e => console.log('fail', e);
 
-const renderSelect = (input, { error, isTouched }) => (
-	<div className="form__field">
-		<select {...input}>
+const renderSelect = (input, { error, isTouched, className }) => (
+	<div className={className}>
+		<select {...input} className={`${className}__input`}>
 			<option value="18-">&lt; 18</option>
 			<option value="18-50">18-50</option>
 			<option value="50+">&gt; 50</option>
 		</select>
 
 		{isTouched && error && (
-			<span className="form__field__error">{error}</span>
+			<span className={`${className}__error`}>{error}</span>
+		)}
+	</div>
+);
+
+const renderRange = (input, { error, isTouched, className }) => (
+	<div className={className}>
+		<input {...input} min="1" max="10" className={`${className}__input`} />
+		{input.value}
+
+		{isTouched && error && (
+			<span className={`${className}__error`}>{error}</span>
 		)}
 	</div>
 );
@@ -32,36 +43,48 @@ const App = () => (
 		onSuccess={handleCommentFormSuccess}
 		onFail={handleCommentFormFail}
 	>
+		<h1 className="form__header">Register</h1>
 		<div className="form__row">
-			<label className="form__row__label">Name</label><br />
+			<label className="form__row__label">Name</label>
 			<Field name="name" validate={[required, minLength(3)]} />
 		</div>
 
 		<div className="form__row">
-			<label className="form__row__label">Email</label><br />
+			<label className="form__row__label">Email</label>
 			<Field type="email" name="email" validate={[required, minLength(3)]} />
 		</div>
 
 		<div className="form__row">
-			<label className="form__row__label">Comment</label><br />
+			<label className="form__row__label">Comment</label>
 			<Field type="textarea" name="comment" validate={required} />
 		</div>
 
 		<div className="form__row">
-			<span className="form__row__label">Gender</span><br />
-			<label>
+			<span className="form__row__label">Gender</span>
+			<label className="form__row__option">
 				<Field name="gender" type="radio" value="male" defaultChecked />
 				Male
 			</label>
-			<label>
+			<label className="form__row__option">
 				<Field name="gender" type="radio" value="female" />
 				Female
 			</label>
 		</div>
 
 		<div className="form__row">
-			<label className="form__row__label">Age</label><br />
+			<label className="form__row__label">Age</label>
 			<Field name="age" component={renderSelect} defaultValue="18-" />
+		</div>
+
+		<div className="form__row">
+			<label className="form__row__label">Programming skill level</label>
+			<Field
+				name="programmingSkill"
+				type="range"
+				defaultValue={5}
+				component={renderRange}
+				transform={value => parseInt(value, 10)}
+			/>
 		</div>
 
 		<div className="form__row">
@@ -71,19 +94,21 @@ const App = () => (
 			</label>
 		</div>
 
-		<WithFormProp isSubmitting>
-			<p>...... I am saving .......</p>
-		</WithFormProp>
+		<div className="form__actions">
+			<WithFormProp isSubmitting>
+				<span>saving...</span>
+			</WithFormProp>
 
-		<WithFormProp hasErrors={false} isSubmitting={false}>
-			<p>...... I have no errors .......</p>
-		</WithFormProp>
+			<WithFormProp hasErrors>
+				<span>Please review your form</span>
+			</WithFormProp>
 
-		<WithFormProp
-			component={({ hasErrors, isSubmitting }) => (
-				<button type="submit" disabled={isSubmitting || hasErrors}>Send</button>
-			)}
-		/>
+			<WithFormProp
+				component={({ hasErrors, isSubmitting }) => (
+					<button className="btn" type="submit" disabled={isSubmitting || hasErrors}>Send</button>
+				)}
+			/>
+		</div>
 	</Form>
 );
 
